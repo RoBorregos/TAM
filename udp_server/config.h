02cc5e6c4efda75cc98318d045e327b9b77b713c
Eme112 <eme_pedraza@hotmail.com>
2023-03-02 17:34:11 -0600

    Created UDP communication files. Not finished yet

---
+++ udp_server/config.h
@@ -0,0 +1,7 @@
+const char* ssid     = "RoBorregos";
+const char* password = "RoBorregos2023";
+const char* hostname = "ESP32_1";
+
+IPAddress ip(192, 168, 1, 200);
+IPAddress gateway(192, 168, 1, 1);
+IPAddress subnet(255, 255, 255, 0);
---
+++ udp_server/udp_server.ino
@@ -0,0 +1,22 @@
+#include <ESP8266WiFi.h>
+#include <WiFiUDP.h>
+
+#include "config.h"  // Sustituir con datos de vuestra red
+#include "UDP.hpp"
+#include "Utils.hpp"
+#include "Utils_UDP.hpp"
+
+void setup() 
+{
+   Serial.begin(115200);
+
+   ConnectWiFi_STA();
+   ConnectUDP();
+}
+
+void loop() 
+{
+   GetUDP_Packet();
+   
+   //SendUDP_Packet("abcde");
+}
